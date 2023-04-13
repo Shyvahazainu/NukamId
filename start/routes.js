a2ffe04916ee8f8314913af5e02786d409315d89
unknown <shyva.b20@mhs.unsyiah.ac.id>
2023-04-14 02:03:47 +0700

    0202

--- start/routes.js
+++ start/routes.js
@@ -17,13 +17,7 @@
 const Route = use('Route')
 
 
-Route.on('/h').render('page/beranda')
+Route.on('/Nukam').render('page/beranda')
 Route.on('/aboutus').render('page/aboutus')
 Route.on('/contact').render('page/contact')
 Route.on('/login').render('page/login')
-
-Route.get('/api/posts', 'PostController.index')
-Route.get('/api/posts/:id', 'PostController.show')
-Route.post('/api/posts', 'PostController.store')
-Route.put('/api/posts/:id', 'PostController.update')
-Route.delete('/api/posts/:id', 'PostController.destroy')
