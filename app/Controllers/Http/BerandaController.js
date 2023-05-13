8e2257470a61c4ba51ad225d74dff45a9d37cfb8
maulidya amanda <lidya_ap@mhs.unsyiah.ac.id>
2023-05-13 17:18:20 +0700

    not-finishyet

--- app/Controllers/Http/BerandaController.js
+++ app/Controllers/Http/BerandaController.js
@@ -1,12 +1,31 @@
-'use strict'
+"use strict";
 
 class BerandaController {
+  index({ view, auth }) {
+    // const user = auth.user.toJSON()
+    const axios = require("axios");
 
-    index({ view, auth }) {
-        const user = auth.user.toJSON()
-        return view.render('page/beranda', { user: user })
-    }
+    // Make a request for a user with a given ID
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/550?api_key=c7c704a0a0bf422b1b19d3fdec8e6f1d"
+      )
+      .then(function (response) {
+        // handle success
+        // return view.render("page/beranda", { response });
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }
 
+  anomali() {
+    return "hai";
+  }
 }
 
-module.exports = BerandaController
+module.exports = BerandaController;
--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*
 |--------------------------------------------------------------------------
@@ -14,27 +14,28 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use("Route");
 
+// Route.on("/beranda").render("page/beranda");
+Route.on("/aboutus").render("page/aboutus");
+Route.on("/contact").render("page/contact");
+Route.on("/register").render("auth/register");
+Route.on("/login").render("auth/login");
+Route.on("/search").render("page/search");
 
-Route.on('/home').render('page/beranda')
-Route.on('/aboutus').render('page/aboutus')
-Route.on('/contact').render('page/contact')
-Route.on('/register').render('auth/register')
-Route.on('/login').render('auth/login')
+// Route.get("/beranda", "BerandaController.index").as("beranda");
+Route.get("/beranda", "BerandaController.index");
 
-Route.get('beranda', 'BerandaController.index'). as('beranda')
+Route.get("register", "Auth/RegisterController.index").as("register.index");
+Route.post("register", "Auth/RegisterController.store").as("register.store");
 
-Route.get('register', 'Auth/RegisterController.index').as('register.index')
-Route.post('register', 'Auth/RegisterController.store').as('register.store')
+Route.get("login", "Auth/LoginController.index").as("login.index");
+Route.post("login", "Auth/LoginController.check").as("login.check");
+Route.get("logout", "Auth/LoginController.logout").as("logout");
 
-Route.get('login', 'Auth/LoginController.index').as('login.index')
-Route.post('login', 'Auth/LoginController.check').as('login.check')
-Route.get('logout', 'Auth/LoginController.logout').as('logout')
-
-Route.get('/posts', 'RegistrationController.index').as('posts.index')
-Route.get('/posts/create', 'RegistrationController.create').as('posts.create')
-Route.post('/posts/store', 'RegistrationController.store').as('posts.store')
+Route.get("/posts", "RegistrationController.index").as("posts.index");
+Route.get("/posts/create", "RegistrationController.create").as("posts.create");
+Route.post("/posts/store", "RegistrationController.store").as("posts.store");
 
 // Route.get('/login', 'RegisterController.index')
 // Route.get('/login/:id', 'RegisterController.show')
